34d21a713ce6fe09e3f0c2e2e10562c6f3654a1f
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-12 15:30:35 +0300

    feat: add proxy route PoC

--- pkg/cloud.ts
+++ pkg/cloud.ts
@@ -6,6 +6,6 @@ export const ory = new V0alpha2Api(
     // for vercel we use the vercel URL
     basePath: process.env.NEXT_PUBLIC_VERCEL_URL
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/.ory`
-      : 'https://localhost:4000/.ory'
+      : 'http://localhost:3000/api/.ory'
   })
 )
--- pkg/open-source.ts
+++ pkg/open-source.ts
@@ -3,6 +3,6 @@ import { Configuration, V0alpha1Api } from '@ory/kratos-client'
 export const ory = new V0alpha1Api(
   new Configuration({
     // This points to the Ory Proxy
-    basePath: 'http://localhost:4000/.ory'
+    basePath: 'http://localhost:3000/api/.ory'
   })
 )
