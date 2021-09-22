ba67e5016cbb55f3ca72a4b648e4366d27daec80
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-22 10:51:39 +0200

    docs: add codedoc for API pages

--- pages/api/.ory/[...paths].ts
+++ pages/api/.ory/[...paths].ts
@@ -1,7 +1,10 @@
+// @ory/integrations offers a package for integrating with NextJS.
 import { nextjs } from '@ory/integrations'
 
+// We need to export the config.
 export const config = nextjs.config
 
+// And create the Ory Cloud API "bridge".
 export default nextjs.createApiHandler({
   fallbackToPlayground: true
 })
