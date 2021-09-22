d393f641cde71ab7422b85289bbf8d4cd5ec9add
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-22 10:51:39 +0200

    docs: add codedoc for API pages

--- pages/api/protected.ts
+++ pages/api/protected.ts
@@ -6,7 +6,7 @@ import { Identity, Session } from '@ory/client'
 
 type Data = {
   session?: Session
-  identity?: Identity
+  fromOryApi?: Identity | string
   error?: AxiosError
 }
 
@@ -14,12 +14,28 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // We use `ory.toSession` and pass the cookies we received to identify the user.
   ory
     .toSession(undefined, req.headers['cookie'])
     .then(({ data: session }) => {
-      ory.adminGetIdentity(session.identity.id).then(({ data: identity }) => {
-        res.status(200).json({ session, identity })
-      })
+      if (!process.env.ORY_ACCESS_TOKEN) {
+        // If the access token is not set, do not fetch the identity from the store.
+        res.status(200).json({ session, fromOryApi: 'To interact with Ory\'s Cloud API please set the ORY_ACCESS_TOKEN environment variable.' })
+      }
+
+      // If you have set up an Ory Cloud Personal Access Token, we use it to fetch the identity from Ory Cloud's API.
+      return ory
+        .adminGetIdentity(session.identity.id)
+        .then(({ data: identity }) => {
+          res.status(200).json({ session, fromOryApi:  identity })
+        })
+        .catch((err: AxiosError) => {
+          res.status(200).json({
+            session,
+            fromOryApi: 'Unable to perform the request.',
+            error: err
+          })
+        })
     })
     .catch((err: AxiosError) => {
       res.status(403).json({
