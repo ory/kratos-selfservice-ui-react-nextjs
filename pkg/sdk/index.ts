55d74b0e29d9b411c55d7b5e444ac3ee5201df2d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-13 13:35:54 +0200

    feat: export helpers from SDK and use kratos-client as primary source

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -3,10 +3,16 @@ import { V0alpha2Api } from '@ory/kratos-client'
 import { ory } from './cloud'
 import { ory as oss } from './open-source'
 
-let exp = ory
+let exp = oss
 
-if (process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC) {
-  exp = oss as any
+if (!process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC) {
+  exp = ory as unknown as V0alpha2Api
 }
+
+export const apiUrl = (
+  process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC ||
+  process.env.ORY_SDK_URL ||
+  ''
+).replace(/\/$/, '')
 
 export default exp
