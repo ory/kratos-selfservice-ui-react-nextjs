17f508cd5253be47613fa09829483bb8986d7dd6
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-13 13:37:09 +0200

    feat: support anchor and script nodes

--- pkg/ui/Node.tsx
+++ pkg/ui/Node.tsx
@@ -1,33 +1,54 @@
-import { UiNode } from '@ory/client'
-
-import { NodeImage } from './NodeImage'
-import { NodeInput } from './NodeInput'
-import { NodeText } from './NodeText'
 import {
+  isUiNodeAnchorAttributes,
   isUiNodeImageAttributes,
   isUiNodeInputAttributes,
+  isUiNodeScriptAttributes,
   isUiNodeTextAttributes
-} from './helpers'
+} from '@ory/integrations/ui'
+import { UiNode } from '@ory/kratos-client'
+
+import { NodeAnchor } from './NodeAnchor'
+import { NodeImage } from './NodeImage'
+import { NodeInput } from './NodeInput'
+import { NodeScript } from './NodeScript'
+import { NodeText } from './NodeText'
+import { FormDispatcher, ValueSetter } from './helpers'
 
 interface Props {
   node: UiNode
   disabled: boolean
   value: any
-  setValue: (value: string | number | boolean) => void
+  setValue: ValueSetter
+  dispatchSubmit: FormDispatcher
 }
 
-export const Node = ({ node, value, setValue, disabled }: Props) => {
+export const Node = ({
+  node,
+  value,
+  setValue,
+  disabled,
+  dispatchSubmit
+}: Props) => {
   if (isUiNodeImageAttributes(node.attributes)) {
     return <NodeImage node={node} attributes={node.attributes} />
+  }
+
+  if (isUiNodeScriptAttributes(node.attributes)) {
+    return <NodeScript node={node} attributes={node.attributes} />
   }
 
   if (isUiNodeTextAttributes(node.attributes)) {
     return <NodeText node={node} attributes={node.attributes} />
   }
 
+  if (isUiNodeAnchorAttributes(node.attributes)) {
+    return <NodeAnchor node={node} attributes={node.attributes} />
+  }
+
   if (isUiNodeInputAttributes(node.attributes)) {
     return (
       <NodeInput
+        dispatchSubmit={dispatchSubmit}
         value={value}
         setValue={setValue}
         node={node}
