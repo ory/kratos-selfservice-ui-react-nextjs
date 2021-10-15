6c2eb646ecf0752f980982a1ee010a2b55f853a0
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-15 19:09:21 +0200

    feat: add cypress e2e tests

---
+++ cypress/integration/pages.spec.js
@@ -0,0 +1,26 @@
+context('Ory Kratos pages', () => {
+  it('can load the login page', () => {
+    cy.visit('/login')
+    cy.get('[name="method"]').should('exist')
+  })
+
+  it('can load the registration page', () => {
+    cy.visit('/registration')
+    cy.get('[name="method"]').should('exist')
+  })
+
+  it('can load the verification page', () => {
+    cy.visit('/verification')
+    cy.get('[name="method"]').should('exist')
+  })
+
+  it('can load the recovery page', () => {
+    cy.visit('/recovery')
+    cy.get('[name="method"]').should('exist')
+  })
+
+  it('can load the welcome page', () => {
+    cy.visit('/')
+    cy.get('h2').should('exist')
+  })
+})
