96a1f814d9eb1efc4433096a8e718140030729e5
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-15 19:09:26 +0200

    fix: resolve SDK loading issues

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,18 +1,12 @@
-import { V0alpha2Api } from '@ory/kratos-client'
+import { V0alpha2ApiInterface } from '@ory/kratos-client'
 
 import { ory } from './cloud'
 import { ory as oss } from './open-source'
 
-let exp = oss
+let exp = ory as unknown as V0alpha2ApiInterface
 
-if (!process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC) {
-  exp = ory as unknown as V0alpha2Api
+if (process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC) {
+  exp = oss
 }
-
-export const apiUrl = (
-  process.env.NEXT_PUBLIC_ORY_KRATOS_PUBLIC ||
-  process.env.ORY_SDK_URL ||
-  ''
-).replace(/\/$/, '')
 
 export default exp
