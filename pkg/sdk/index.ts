a2e04ff414c17bf052b546f40f99e4d8c17d3650
Vinh Hoi Le Chau <vinh.lechau@missmp.eu>
2022-07-22 20:32:39 +0700

    Fix fix

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,6 +1,8 @@
 import { edgeConfig } from '@ory/integrations/next'
 import { Configuration, V0alpha2Api } from '@ory/kratos-client'
 
-export default new V0alpha2Api(new Configuration({
-    basePath: "https://auth.missmp.tech/ui/";
-}))
+export default new V0alpha2Api(
+  new Configuration({
+    basePath: 'https://auth.missmp.tech/ui/'
+  })
+)
