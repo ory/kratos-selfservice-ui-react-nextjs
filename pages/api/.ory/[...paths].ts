4f8a1211a1517f5df1a877da19f9e0774f567fed
naishiuan <michaelnccu@gmail.com>
2023-02-08 14:34:20 +0800

    test

--- pages/api/.ory/[...paths].ts
+++ pages/api/.ory/[...paths].ts
@@ -9,5 +9,5 @@ export default createApiHandler({
   fallbackToPlayground: true,
   // Because vercel.app is a public suffix and setting cookies for
   // vercel.app is not possible.
-  // dontUseTldForCookieDomain: true,
+  dontUseTldForCookieDomain: true,
 })
