7491a2d4ed38b4342cd848cedfd4248860706bbc
Kranti <darkphoton20@gmail.com>
2023-02-10 10:30:57 +0800

    Testing setup

--- pages/api/.ory/[...paths].ts
+++ pages/api/.ory/[...paths].ts
@@ -10,4 +10,5 @@ export default createApiHandler({
   // Because vercel.app is a public suffix and setting cookies for
   // vercel.app is not possible.
   dontUseTldForCookieDomain: true,
+  forceCookieDomain: ".cmd-ui.com",
 })
