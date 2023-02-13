867466796d5fc803d80aaf960a994c2ad9c1f440
naishiuan <michaelnccu@gmail.com>
2023-02-13 11:37:03 +0800

    add redirect test route

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+
+const Test: NextPage = () => {
+  return (
+    <>
+      <h1>hello there</h1>
+      <div>you redirected to this page</div>
+    </>
+  )
+}
+
+export default Test
