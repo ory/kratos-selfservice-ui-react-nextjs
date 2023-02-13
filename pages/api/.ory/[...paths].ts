9bf5c54910e2ab66da7847b83abc13ee2afec61e
naishiuan <michaelnccu@gmail.com>
2023-02-13 15:47:13 +0800

    uncomment forceCookieDomain

--- pages/api/.ory/[...paths].ts
+++ pages/api/.ory/[...paths].ts
@@ -9,5 +9,5 @@ export default createApiHandler({
   fallbackToPlayground: true,
   // Because vercel.app is a public suffix and setting cookies for
   // vercel.app is not possible.
-  // forceCookieDomain: ".cmid-ui.com",
+  forceCookieDomain: ".cmid-ui.com",
 })
