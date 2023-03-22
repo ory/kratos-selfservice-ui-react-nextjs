a202fddde0813da484b5d866059af821619c521d
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-03-22 17:33:52 +0100

    feat: show verification UI after registration (#55)

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import { ReactNode, useEffect, useState } from "react"
 
-import { Flow, Methods, Messages, ActionCard, CenterLink } from "../pkg"
+import { ActionCard, CenterLink, Flow, Messages, Methods } from "../pkg"
 import { handleFlowError } from "../pkg/errors"
 import ory from "../pkg/sdk"
 
@@ -85,6 +85,18 @@ const Settings: NextPage = () => {
           .then(({ data }) => {
             // The settings have been saved and the flow was updated. Let's show it to the user!
             setFlow(data)
+
+            // continue_with is a list of actions that the user might need to take before the settings update is complete.
+            // It could, for example, contain a link to the verification form.
+            if (data.continue_with) {
+              for (const item of data.continue_with) {
+                switch (item.action) {
+                  case "show_verification_ui":
+                    router.push("/verification?flow=" + item.flow.id)
+                    return
+                }
+              }
+            }
           })
           .catch(handleFlowError(router, "settings", setFlow))
           .catch(async (err: AxiosError) => {
