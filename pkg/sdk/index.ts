e13b91bce0a168b71b1e2b9e9538c4a2f4b42fb0
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-10-02 11:50:32 +0200

    feat: prevent proxy when specifying `NEXT_PUBLIC_ORY_KRATOS_URL`

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,4 +1,12 @@
 import { Configuration, FrontendApi } from "@ory/client"
 import { edgeConfig } from "@ory/integrations/next"
 
-export default new FrontendApi(new Configuration(edgeConfig))
+const localConfig = {
+  basePath: process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL,
+}
+
+export default new FrontendApi(
+  new Configuration(
+    process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL ? localConfig : edgeConfig,
+  ),
+)
