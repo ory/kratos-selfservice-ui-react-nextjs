a1bfc2842d68d73f0508e319626e59a8e529f0d1
hackerman <3372410+aeneasr@users.noreply.github.com>
2024-02-22 13:37:22 +0100

    fix: withCredentials missing for self-hosting (#67)

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -3,6 +3,9 @@ import { edgeConfig } from "@ory/integrations/next"
 
 const localConfig = {
   basePath: process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL,
+  baseOptions: {
+    withCredentials: true,
+  },
 }
 
 export default new FrontendApi(
