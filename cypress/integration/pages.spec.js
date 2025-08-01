15d1ee6efc22d50a63ef1cbb3e2d2a365a10eade
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-08-01 03:54:46 -0400

    revert: tests: improve randomness in e2e tests
    GitOrigin-RevId: 377b6b2dca8eec59f244d3b0f94883a6b443b772

--- cypress/integration/pages.spec.js
+++ cypress/integration/pages.spec.js
@@ -1,4 +1,11 @@
+const randomString = () => (Math.random() + 1).toString(36).substring(7)
+const randomPassword = () => randomString() + randomString()
+const randomEmail = () => randomString() + "@" + randomString() + ".com"
+
 context("Ory Kratos pages", () => {
+  const email = randomEmail()
+  const password = randomPassword()
+
   beforeEach(() => {
     cy.clearCookies({ domain: null })
   })
